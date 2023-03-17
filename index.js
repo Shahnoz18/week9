1b8ce10614a7678c7df792b5bf58c310fde8cff5
Shahnoz18 <shob181296@mail.ru>
2023-03-17 16:38:17 +0300

    comment

---
+++ index.js
@@ -0,0 +1,43 @@
+const cleaning = 40000;
+const vocal = 22000;
+const cleanVocal = cleaning + vocal;
+console.log(`Ответ на 1 вопрос: ${cleanVocal} рублей будет зарабатывать уборщик, имея голос и пед. образование`);
+
+const word = "Владивосток";
+const number = 2000;
+const wordNumber = word + number;
+console.log(`Ответ на 2 вопрос: ${wordNumber}`);
+
+const messendjer = 128;
+const like = 254;
+const vk = 137;
+const youTube = 201;
+const minHour = 60;
+const messLikeVkYouTube = messendjer + like + vk + youTube;
+const hour = messLikeVkYouTube / minHour;
+console.log(`Ответ на 3 вопрос: ${hour} часов`);
+
+const x = 2;
+const y = 5;
+console.log(`Ответ на 4 вопрос: ${x**y}`);
+
+const text = `"Нужно бежать со всех ног, чтобы только оставаться на месте, а чтобы куда-то попасть, надо бежать как минимум вдвое быстрее!"`;
+console.log(`Ответ на 5 вопрос: ${text}`);
+
+const greetings = "Привет";
+const znak = ","
+const probel = " ";
+const name = "Кот"; 
+const gretName = greetings + znak + probel + name;
+console.log(`Ответ на 6 вопрос: ${gretName}`);
+
+let time = 34;
+
+console.log(time);
+console.log('Старт поездки. Осталось минут: ' + time);
+let time1 = 19;
+console.log('Немного сторис в соцсетях. Осталось минут:' + time1);
+let time2 = 9;
+console.log('Немного не новостей, но событий. Осталось минут:' + time2);
+let time3 = 0;
+console.log("Вы приехали.Добро пожаловать в Москву.");
